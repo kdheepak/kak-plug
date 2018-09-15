06c504efd01aaf7fe2b08407cc15d8845713df54
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-09-14 22:30:19 -0600

    Move to kak_plug folder

---
+++ kak_plug/kak_plug.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+curdir = os.path.dirname(os.path.realpath(__file__))
+
+formatter = logging.Formatter(
+    "%(asctime)s [%(levelname)s] - %(message)s", datefmt="%Y-%m-%d %I:%M:%S %p"
+)
+
+h = logging.StreamHandler()
+h.setFormatter(formatter)
+logger.addHandler(h)
+
+h = logging.FileHandler(os.path.join(curdir, "plug.log"), "w")
+h.setFormatter(formatter)
+logger.addHandler(h)
+logger.setLevel(logging.DEBUG)
+
+logger.debug("Current directory: {}".format(curdir))
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+
+    main()
